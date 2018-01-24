a11c933e67b2a1236a43ccd2225fa85ebdd41a09
michele donini <mikko108382892@gmail.com>
2018-01-24 09:32:32 +0100

    debug

--- experiments.py
+++ experiments.py
@@ -67,7 +67,8 @@ def balanced_accuracy_score(y_true, y_pred, sample_weight=None):
 
 if __name__ == '__main__':
     # Experimental settings
-    experiment_number = 12
+    # 12, 8, 2, 13, 14
+    experiment_number = 2
     smaller_option = False
     accuracy_balanced = False
     verbose = 3
